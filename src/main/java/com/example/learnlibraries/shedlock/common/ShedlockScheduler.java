8fccbd186ca1b46cdd2020d77502756d2687c75c
jeonhyunseok <sjunhs@gmail.com>
2023-04-25 21:57:51 +0900

    feature : shedlock - enable lock

--- src/main/java/com/example/learnlibraries/shedlock/common/ShedlockScheduler.java
+++ src/main/java/com/example/learnlibraries/shedlock/common/ShedlockScheduler.java
@@ -2,11 +2,14 @@ package com.example.learnlibraries.shedlock.common;
 
 import com.example.learnlibraries.shedlock.component.DBInsertComponent;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@EnableSchedulerLock(defaultLockAtMostFor = "PT9S")
 public class ShedlockScheduler {
 
     private final DBInsertComponent dbInsertComponent;
@@ -16,6 +19,7 @@ public class ShedlockScheduler {
     }
 
     @Scheduled(cron = "*/10 * * * * ?")
+    @SchedulerLock(name="TestLock", lockAtLeastFor = "PT5S", lockAtMostFor = "PT9S")
     public void shedlockTester() {
         log.info("call scheduler");
         dbInsertComponent.insert();
